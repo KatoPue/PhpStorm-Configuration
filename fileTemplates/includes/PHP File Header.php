42d594698d7f5c185a24ac03997b59faf8c458b1
RayEhrhardt <ray.ehrhardt@gmail.com>
2018-02-02 14:11:26 +0100

    Current state of my PhpStorm configuration

---
+++ fileTemplates/includes/PHP File Header.php
@@ -0,0 +1,9 @@
+declare(strict_types=1);
+
+/**
+ * This file is part of ${PROJECT_NAME}.
+ * (c) author name <author_email@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
